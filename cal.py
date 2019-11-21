ec4d7653bf8548ea7dd60cb1044ec75238a3a058
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2019-11-21 16:05:53 +0200

    Modified ui.py and cal.py

--- cal.py
+++ cal.py
@@ -1,1 +1,53 @@
 # main program
+import sys
+import ui
+
+meetings_dic = {'8': '', '9': '', '10': '', '11': '', '12': '', '13': '', '14': '', '15': '', '16': '',
+                '17': '', '18': '', '19': ''}
+
+
+def schedule_meeting():
+    global meetings_dic
+    inputs = ui.get_inputs(
+        ["Enter meeting title: ", "Enter duration in hours (1 or 2): ", "Enter start time: "], "")
+    for i in meetings_dic.keys():
+        if inputs[2] == i and inputs[1] == '1':
+            meetings_dic[i] = inputs[0]
+        elif inputs[2] == i and inputs[1] == '2':
+            meetings_dic[i] = inputs[0]
+            meetings_dic[str(int(i)+1)] = inputs[0]
+
+
+def choose():
+    inputs = ui.get_inputs(["Please enter an option: "], "")
+    option = inputs[0]
+    if option == "s":
+        schedule_meeting()
+        print(meetings_dic)
+    elif option == "c":
+        hr.start_module()
+    elif option == "q":
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such option.")
+
+
+def handle_menu():
+    options = ["schedule a new meeting",
+               "cancel an existing meeting",
+               "quit"]
+
+    ui.print_menu("Main menu", options, "quit")
+
+
+def main():
+    while True:
+        handle_menu()
+        try:
+            choose()
+        except KeyError as err:
+            ui.print_error_message(str(err))
+
+
+if __name__ == '__main__':
+    main()
--- ui.py
+++ ui.py
@@ -1,1 +1,54 @@
 # printing data, asking user for input
+
+
+def print_menu(title, list_options, exit_message):
+    print(f'    {title}')
+    letter_list = ['s', 'c', 'q']
+    i = 1
+    while i < len(list_options):
+        print(f'        ({letter_list[i-1]}) {list_options[i-1]}')
+        i += 1
+    print(f'        (q) {exit_message}')
+
+
+def get_inputs(list_labels, title):
+    """
+    Gets list of inputs from the user.
+    Sample call:
+        get_inputs(["Name","Surname","Age"],
+                    "Please provide your personal information")
+    Sample display:
+        Please provide your personal information
+        Name <user_input_1>
+        Surname <user_input_2>
+        Age <user_input_3>
+
+    Args:
+        list_labels (list): labels of inputs
+        title (string): title of the "input section"
+
+    Returns:
+        list: List of data given by the user. Sample return:
+            [<user_input_1>, <user_input_2>, <user_input_3>]
+    """
+
+    i = 0
+    inputs = []
+    while i < len(list_labels):
+        inputs.append(input(f'{title} {list_labels[i]}'))
+        i += 1
+    return inputs
+
+
+def print_error_message(message):
+    """
+    Displays an error message (example: ``Error: @message``)
+
+    Args:
+        message (str): error message to be displayed
+
+    Returns:
+        None: This function doesn't return anything it only prints to console.
+    """
+
+    print(f'Error - {message}')
